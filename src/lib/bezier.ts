ec14de6e6da400666985796e8be4e07ab68d1d7d
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-08-25 11:47:29 -0500

    feat: make path duration more reasonable
    - path duration is now 0.005 * arclength(path) + 2

--- src/lib/bezier.ts
+++ src/lib/bezier.ts
@@ -37,3 +37,34 @@ function quadraticToCubicBezier(startPoint: Point, controlPoint: Point, endPoint
     }
   ];
 }
+
+export function cubicBezierChainLength(points: Point[]): number {
+  let length = 0;
+  for (let i = 0; i < points.length - 3; i += 3) {
+    const p0 = points[i];
+    const p1 = points[i + 1];
+    const p2 = points[i + 2];
+    const p3 = points[i + 3];
+    length += cubicBezierLength(p0, p1, p2, p3);
+  }
+  return length;
+}
+
+function cubicBezierLength(p0: Point, p1: Point, p2: Point, p3: Point): number {
+  const STEPS = 100;
+  const STEP = 1 / STEPS;
+
+  let length = 0;
+  for (let t1 = 0; t1 < 1; t1 += STEP) {
+    const t2 = t1 + STEP;
+
+    const x1 = (1 - t1) ** 3 * p0.x + 3 * (1 - t1) ** 2 * t1 * p1.x + 3 * (1 - t1) * t1 * p2.x + t1 ** 3 * p3.x;
+    const y1 = (1 - t1) ** 3 * p0.y + 3 * (1 - t1) ** 2 * t1 * p1.y + 3 * (1 - t1) * t1 * p2.y + t1 ** 3 * p3.y;
+    const x2 = (1 - t2) ** 3 * p0.x + 3 * (1 - t2) ** 2 * t2 * p1.x + 3 * (1 - t2) * t2 * p2.x + t2 ** 3 * p3.x;
+    const y2 = (1 - t2) ** 3 * p0.y + 3 * (1 - t2) ** 2 * t2 * p1.y + 3 * (1 - t2) * t2 * p2.y + t2 ** 3 * p3.y;
+
+    const segmentLength = Math.hypot(x2 - x1, y2 - y1);
+    length += segmentLength;
+  }
+  return length;
+}
--- src/routes/auto/[id]/PathChainDisplay.svelte
+++ src/routes/auto/[id]/PathChainDisplay.svelte
@@ -3,7 +3,7 @@
   import BezierPathChain from "$lib/components/curves/BezierPathChain.svelte";
   import { TimeManager } from "$lib/TimeManager.svelte.js";
   import type { PathChain, Point } from "$lib/pathChains";
-  import { pathChainToCubicBezierChain } from "$lib/bezier";
+  import { cubicBezierChainLength, pathChainToCubicBezierChain } from "$lib/bezier";
   import { onMount } from "svelte";
   import { gsap } from "gsap";
   import { MotionPathPlugin } from "gsap/MotionPathPlugin";
@@ -31,6 +31,9 @@
     });
   });
 
+  const PATH_DURATION_SCALAR = 0.005;
+  const PATH_DURATION_MIN = 2;
+
   $effect(() => {
     timeManager.timeline.killTweensOf(robot);
     timeManager.timeline.to(
@@ -42,7 +45,7 @@
           alignOrigin: [0.5, 0.5],
           fromCurrent: false
         },
-        duration: 3,
+        duration: PATH_DURATION_SCALAR * cubicBezierChainLength($state.snapshot(cubicBezierChain)) + PATH_DURATION_MIN,
         ease: "sine.inOut"
       },
       0
@@ -72,12 +75,6 @@
   <svg role="region" onmousemove={onSvgMouseMove} onmouseleave={onSvgMouseLeave}
        class="absolute top-0 left-0 h-full w-full" viewBox="0 0 144 144">
     <BezierPathChain bind:pathChain timer={timeManager} bind:robotVisible />
-<!--    <rect-->
-<!--      bind:this={robot}-->
-<!--      width="18"-->
-<!--      height="18"-->
-<!--      class="fill-primary z-99 {robotVisible ? hoveringOverRobot ? 'opacity-50' : '' : 'hidden'} pointer-events-none"-->
-<!--    />-->
     <image bind:this={robot} href="/images/robot.png" width="18" height="18"
            class="z-99 {robotVisible ? hoveringOverRobot ? 'opacity-80' : '' : 'hidden'} pointer-events-none" />
   </svg>
