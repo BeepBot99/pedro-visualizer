a5b6a4068dbbe01d119f00598c325f2ce5f79d27
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-09-02 10:36:28 -0500

    feat: add lock/unlock handles button in WaypointEditor

--- src/lib/components/configuration/WaypointEditor.svelte
+++ src/lib/components/configuration/WaypointEditor.svelte
@@ -125,7 +125,30 @@
       {/if}
       <div class="flex justify-between items-center my-4">
         <span>Anchor</span>
-        <PointEditor bind:point={waypoint.point} />
+        <div class="flex gap-2">
+          {#if waypoint.handleBefore && waypoint.handleAfter}
+            {#if waypoint.smooth}
+              <ButtonUI onclick={() => waypoint.smooth = false} name="Unlock Handles"
+                        Class="text-base-content/50 p-1! m-0!">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                </svg>
+              </ButtonUI>
+            {:else}
+              <ButtonUI onclick={() => waypoint.smooth = true} name="Lock Handles"
+                        Class="text-base-content/50 p-1! m-0!">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                </svg>
+              </ButtonUI>
+            {/if}
+          {/if}
+          <PointEditor bind:point={waypoint.point} />
+        </div>
       </div>
       {#if !isEndPoint}
         <div class="flex justify-between items-center my-4">
@@ -159,6 +182,8 @@
     </div>
   {/if}
   <div class="flex items-center justify-center">
-    <ButtonUI onclick={onAddAfter} Class="m-4 border-base-content! hover:bg-base-content/10" name="Add Waypoint After">Add Waypoint After</ButtonUI>
+    <ButtonUI onclick={onAddAfter} Class="m-4 border-base-content! hover:bg-base-content/10" name="Add Waypoint After">
+      Add Waypoint After
+    </ButtonUI>
   </div>
 </div>
